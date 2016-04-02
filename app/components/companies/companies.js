5ed4a1a544b737de8d2438f04c336894bca4cd49
Jason Maurer <j@maur.co>
2016-04-01 23:02:30 -0600

    company list

--- app/components/companies/companies.js
+++ app/components/companies/companies.js
@@ -4,14 +4,36 @@ import styles from './companies.styl'
 
 class Companies extends React.Component {
   render() {
-    let stocks = [
+    let companies = [
       { name: 'APPL', shares: '120', price: '$105.67', status: 'up' },
       { name: 'FB', shares: '110', price: '$113.05', status: 'up' },
-      { name: 'TWTR', shares: '80', price: '$15.91', status: 'down' }
+      { name: 'TWTR', shares: '80', price: '$15.91', status: 'down' },
+      { name: 'TSLA', shares: '75', price: '$227.75', status: 'up' },
+      { name: 'NFLX', shares: '50', price: '$98.36', status: 'down' },
+      { name: 'F', shares: '50', price: '$13.06', status: 'down' },
+      { name: 'DIS', shares: '15', price: '$97.22', status: 'up' },
+      { name: 'GPRO', shares: '10', price: '$12.42', status: 'up' },
+      { name: 'FIT', shares: '10', price: '$14.58', status: 'up' },
+      { name: 'BABA', shares: '5', price: '$75.86', status: 'down' }
     ]
 
-    stocks = stocks.map((stock, key) => {
-      return <li key={ key }>{ stock.name }</li>
+    companies = companies.map((company, key) => {
+      const classname = classnames(styles.company, {
+        [styles.companyActive]: (key === 2)
+      })
+
+      return (
+        <div key={ key } className={ classname }>
+          <div className={ styles.companyName }>
+            <h3>{ company.name }</h3>
+            <h5>{ company.shares } SHARES</h5>
+          </div>
+
+          <span className={ company.status === 'up' ? styles.priceUp : styles.priceDown }>
+            { company.price }
+          </span>
+        </div>
+      )
     })
 
     return (
@@ -21,9 +43,9 @@ class Companies extends React.Component {
           <i className="fa fa-search"></i>
         </div>
 
-        <ul className={ styles.stocks }>
-          { stocks }
-        </ul>
+        <nav className={ styles.companies }>
+          { companies }
+        </nav>
       </div>
     )
   }
